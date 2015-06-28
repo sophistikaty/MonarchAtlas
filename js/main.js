dbb4662695b6bb0c1511025ee4283f90d2cbcece
sophistikaty <sophistikaty@me.com>
2015-06-28 19:35:26 -0400

    carousel bug fix7?

--- js/main.js
+++ js/main.js
@@ -41,15 +41,18 @@ $(document).ready(function(){
 		next = document.getElementsByClassName('icon-left-open-big');
 
 	$('#carousel').slick({
-      // slidesToShow: 1,
+      
 	  autoplay: true,
+	  autoplaySpeed: 4000,
+	  pauseOnHover: false,
 	  infinite: true,
 	  speed: 500,
 	  fade: true,
 	  cssEase: "linear",
 	  respondTo: slider,
 	  prevArrow: "<div class='icon-left-open-big slick-prev'></div>",
-	  nextArrow: "<div class='icon-right-open-big slick-next'></div>"
+	  nextArrow: "<div class='icon-right-open-big slick-next'></div>",
+	  centerMode: true
 	  
 	  
 	 //  responsive: [
